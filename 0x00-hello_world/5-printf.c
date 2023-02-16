3bfb625e0f22e453a72a84b12e945667adf63491
jendoakino <jendoakino@gmail.com>
2023-02-16 08:24:03 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,10 @@
  * main - A program that print a line using printf function
  * Return: 0 (success)
  */
+
 int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-
+	
