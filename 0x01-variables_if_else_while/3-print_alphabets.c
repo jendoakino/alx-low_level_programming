129f905242b647656f9a85b2344d31d63be40eec
jendoakino <jendoakino@gmail.com>
2023-02-18 10:55:18 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower and upper case
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
