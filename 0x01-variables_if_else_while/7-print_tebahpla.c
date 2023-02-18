758ea7c276e34fb6496632adc3b6a0970de3b2dc
jendoakino <jendoakino@gmail.com>
2023-02-18 13:54:54 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in reverse
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'z'; a =>  'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
