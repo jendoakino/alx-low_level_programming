2de77bd1e52283e3e0152723daf7d83a8f1e8b37
jendoakino <jendoakino@gmail.com>
2023-02-19 09:14:19 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible outcomes of combination of two digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int c, i;
+
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
+
+
+				if (c != '8' || (c == '8' && i != '9'))
+
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
