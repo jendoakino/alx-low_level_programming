1efa4e38164099dc72680ba588de775208fef103
jendoakino <jendoakino@gmail.com>
2023-02-22 11:39:36 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - print the sign of a number
+ * @n: the number to be checked
+ * Return: 1 for positive number, -1 for negative number and 0 for others
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return  (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return  (0);
+	}
+	else
+	{
+		_putchar('-');
+		return  (-1);
+	}
+}
