1976fe20178d8ed0e1c1be375f4ac4dfa5c9cfe7
jendoakino <jendoakino@gmail.com>
2023-02-23 10:33:53 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - test functions that prints if integer is positive or negative
  * Return: 0
@@ -7,9 +8,7 @@
 int main(void)
 {
 	int i;
-
 	i = 0;
-
 	positive_or_negative(i);
 
 	return  (0);
