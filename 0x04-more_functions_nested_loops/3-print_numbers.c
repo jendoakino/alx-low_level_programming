a41ebeb1718b23ecf1559db4403f6305cd68f40f
jendoakino <jendoakino@gmail.com>
2023-02-24 07:49:03 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number from 0 - 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; c < 58; c++)
+
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+
+}
+
