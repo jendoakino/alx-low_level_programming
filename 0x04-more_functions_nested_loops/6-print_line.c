3b466e6b6227eaf3270362d2144f7c4c83de497d
jendoakino <jendoakino@gmail.com>
2023-02-24 17:38:03 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+
+			_putchar(95);
+		_putchar('\n');
+	}
+}
