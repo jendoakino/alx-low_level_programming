625eda8abe7bc501ff83476be2920d94280ebf72
jendoakino <jendoakino@gmail.com>
2023-03-03 18:13:03 -0800

    task six

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * *cap_string -  function that capitalizes all words of a string
+ * *cap_string - capitalizes all words of a string
  * @str: - string
- *Return:pointer to string
+ *Return: pointer to string
  */
 char *cap_string(char *str)
 {
 int i = 0;
 
- while (str[i])
+while (str[i])
 {
 while (!(str[i] >= 'a' && str[i] <= 'z'))
 i++;
@@ -31,5 +31,5 @@ str[i] -= 32;
 
 i++;
 }
- return (str);
+return (str);
 }
