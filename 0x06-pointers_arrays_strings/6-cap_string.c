05618d79bdf96b2099c8fc0e60d9c47ccf76a0d7
jendoakino <jendoakino@gmail.com>
2023-03-03 18:02:31 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,10 +6,10 @@
  *Return:pointer to string
  */
 char *cap_string(char *str)
-}
+{
 int i = 0;
 
-while (str[i])
+ while (str[i])
 {
 while (!(str[i] >= 'a' && str[i] <= 'z'))
 i++;
@@ -31,6 +31,5 @@ str[i] -= 32;
 
 i++;
 }
-return (str);
+ return (str);
 }
-
